e118063fac6301172af96219d9a9bebeead226ef
ivy-nguyen18 <nguyen.ivy1802@knights.ucf.edu>
2021-06-16 16:21:02 -0400

    Initial commit

---
+++ src/main/java/oop/assignment3/ex41/base/App.java
@@ -0,0 +1,7 @@
+package oop.assignment3.ex41.base;
+
+import java.util.Scanner;
+
+public class App {
+
+}
---
+++ src/test/java/oop/assignment3/AppTest.java
@@ -0,0 +1,20 @@
+package oop.assignment3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
